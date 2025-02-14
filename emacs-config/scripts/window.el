b9d485d238263a2d5ecc47fbc0b276b7817957f6
zaine <zaineulabideen@outlook.com>
2025-02-14 15:07:45 +0000

    changed the emacs config file - for window movement

--- emacs-config/scripts/window.el
+++ emacs-config/scripts/window.el
@@ -1,69 +1,85 @@
+;;; window.el --- Easily swap windows in Emacs -*- lexical-binding: t; -*-
+
+;; Author: Zaine
+;; Version: 1.0
+;; URL: https://github.com/zainezq/dot-files/
+
+;;; Commentary:
+
+;; This package provides convenient functions for swapping buffers between windows.
+;; It allows users to move buffers up, down, left, or right across window splits.
+;; The functions rely on `windmove` for navigation.
+
+;;; Code:
+
 (require 'windmove)
+(require 'window)
+
+;; ----------------------------------------------------------------------------
+;; Buffer Movement Functions
+;; ----------------------------------------------------------------------------
 
 ;;;###autoload
 (defun buf-move-up ()
   "Swap the current buffer and the buffer above the split.
-If there is no split, ie now window above the current one, an
-error is signaled."
-;;  "Switches between the current buffer, and the buffer above the
-;;  split, if possible."
+If there is no split, an error is signaled."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'up))
-	 (buf-this-buf (window-buffer (selected-window))))
+         (buf-this-buf (window-buffer (selected-window))))
     (if (null other-win)
         (error "No window above this one")
-      ;; swap top with this one
+      ;; Swap buffers
       (set-window-buffer (selected-window) (window-buffer other-win))
-      ;; move this one to top
       (set-window-buffer other-win buf-this-buf)
       (select-window other-win))))
 
 ;;;###autoload
 (defun buf-move-down ()
-"Swap the current buffer and the buffer under the split.
-If there is no split, ie now window under the current one, an
-error is signaled."
+  "Swap the current buffer and the buffer below the split.
+If there is no split, an error is signaled."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'down))
-	 (buf-this-buf (window-buffer (selected-window))))
-    (if (or (null other-win) 
+         (buf-this-buf (window-buffer (selected-window))))
+    (if (or (null other-win)
             (string-match "^ \\*Minibuf" (buffer-name (window-buffer other-win))))
-        (error "No window under this one")
-      ;; swap top with this one
+        (error "No window below this one")
+      ;; Swap buffers
       (set-window-buffer (selected-window) (window-buffer other-win))
-      ;; move this one to top
       (set-window-buffer other-win buf-this-buf)
       (select-window other-win))))
 
 ;;;###autoload
 (defun buf-move-left ()
-"Swap the current buffer and the buffer on the left of the split.
-If there is no split, ie now window on the left of the current
-one, an error is signaled."
+  "Swap the current buffer and the buffer on the left of the split.
+If there is no split, an error is signaled."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'left))
-	 (buf-this-buf (window-buffer (selected-window))))
+         (buf-this-buf (window-buffer (selected-window))))
     (if (null other-win)
-        (error "No left split")
-      ;; swap top with this one
+        (error "No window to the left")
+      ;; Swap buffers
       (set-window-buffer (selected-window) (window-buffer other-win))
-      ;; move this one to top
       (set-window-buffer other-win buf-this-buf)
       (select-window other-win))))
 
 ;;;###autoload
 (defun buf-move-right ()
-"Swap the current buffer and the buffer on the right of the split.
-If there is no split, ie now window on the right of the current
-one, an error is signaled."
+  "Swap the current buffer and the buffer on the right of the split.
+If there is no split, an error is signaled."
   (interactive)
   (let* ((other-win (windmove-find-other-window 'right))
-	 (buf-this-buf (window-buffer (selected-window))))
+         (buf-this-buf (window-buffer (selected-window))))
     (if (null other-win)
-        (error "No right split")
-      ;; swap top with this one
+        (error "No window to the right")
+      ;; Swap buffers
       (set-window-buffer (selected-window) (window-buffer other-win))
-      ;; move this one to top
       (set-window-buffer other-win buf-this-buf)
       (select-window other-win))))
-(require 'window)
+
+;; ----------------------------------------------------------------------------
+;; Provide
+;; ----------------------------------------------------------------------------
+
+(provide 'window)
+
+;;; window.el ends here
